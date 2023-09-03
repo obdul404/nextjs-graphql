659cb77e61227733e1d3b87b02fad8b22defd9ed
Abdul Hannan <abdul@visualr.com.au>
2023-09-04 00:28:18 +0500

    Read users

---
+++ graphql/queries.ts
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const GET_USERS = gql`
+    query Users {
+        users {
+        id
+        email
+        name
+        role
+        }
+    }
+`;
